eb39d2df374524f0c0285bc34bbd64906d7d7920
candezunino <candezunino@gmail.com>
2021-09-18 15:46:08 -0300

    Create files .py

--- core/etl/landing_to_raw.py
+++ core/etl/landing_to_raw.py
@@ -1,3 +1,38 @@
-def main_execution():
-    return "Hello World!"
+from collections import OrderedDict #to order the dictionary like the given example
+import csv,json
+
+def landing_to_raw(url_csv,url_json='../../db/raw/unknown.json'):
+
+    KEYS_TO_REPARSE = ["billing_address", "client_details", "customer", 
+                        "fulfillments", "line_items", "payment_details", 
+                        "shipping_address", "shipping_lines", "transactions"]
+
+    result=OrderedDict()
+    data=[]
+
+    #Open CSV file. Only Read access.
+    with open(url_csv,'r') as csvFile:
+        reader = csv.DictReader(csvFile)
+        for row in reader:
+            
+            # Parse the keys that I need to and are not empty
+            new_line = {k: json.loads(v) if k in KEYS_TO_REPARSE and v != "" else v for k, v in row.items()}
+
+            #Removes the metadata and saves it in a new index with the names that we need (the cols should always be the same).
+            ## I use pop so the data_raw can have data of the rest of the columns, without the metadata
+            ##it doesn't matter if the columns change.
+            result["daton_user_id"] = new_line.pop('_job_batch_id')
+            result["daton_batch_runtime"] = new_line.pop('_job_batch_runtime')
+            result["daton_batch_id"] = new_line.pop('_job_user_id')
+            result["creation_ts"] = new_line.pop('updated_at')
+            result["data_raw"] = new_line
+
+            #Order the dictionary so it can be like the example
+            result.move_to_end('data_raw',last=False)
+            data.append(result)
+        
+        
+    #Open TXT file. Write access. It starts appending the dictionaries in the first iteration.
+    with open(url_json,'w') as jsonFile:
+        json.dump(data, jsonFile)
 
--- core/etl/raw_to_app.py
+++ core/etl/raw_to_app.py
@@ -1,3 +1,40 @@
-def main_execution():
-    return "Hello World!"
+import csv,json
+import pandas as pd
+
+def raw_to_app(table_name,url_json='../../db/raw/unknown.json'):
+    with open(url_json) as f:
+        data = json.loads(f.read())
+
+    ###Map detailed table
+    data_raw = [row["data_raw"] for row in data]
+
+    #Export the columns of transactions needed
+    df_transactions =  pd.json_normalize(data_raw,'transactions')
+    df_ids=df_transactions.filter(['id','order_id'],axis=1).astype(int)
+
+    #Export the columns of data_raw needed
+    df_draw = pd.json_normalize(data_raw)
+    df_price=df_draw.filter(['user_id','total_price_usd','total_price'],axis=1).astype({'user_id':int,'total_price_usd':float,'total_price':float})
+
+    #Export the columns of general json
+    df = pd.json_normalize(data)
+    df_creation=df.filter(['creation_ts'],axis=1)
+    df_creation=pd.to_datetime(df_creation.creation_ts)
+
+
+    df_dim=pd.concat([df_ids,df_price,df_creation],axis=1)
+
+    df_dim.to_parquet('../../db/app/dim_'+table_name+'.gzip',
+                compression='gzip') 
+ 
+    ###Metrics table
+    df_orders=df_transactions.filter(['created_at','order_id','amount'],axis=1).astype({'order_id':int,'amount':float})
+
+    df_orders['created_at']=pd.to_datetime(df_orders['created_at']).dt.date
+
+    df_metrics=df_orders.groupby('created_at', as_index=False).agg({'order_id':'count','amount': 'sum'})
+    df_metrics=df_metrics.rename(columns={'created_at': 'creation_dt', 'order_id': 'total_orders_qty','amount':'total_sales_amount'})
+
+    df_metrics.to_parquet('../../db/app/agg_'+table_name+'.gzip',
+                compression='gzip') 
 
